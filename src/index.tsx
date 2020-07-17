d7f54f386bf79a1d6843e339798cf787a62825a8
Heather Stoffels <Umdstarlet@gmail.com>
2020-07-17 12:20:14 -0500

    fetching data from API

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+// import API from "./API";
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/utils.tsx
@@ -0,0 +1,8 @@
+
+import React from "react";
+
+const utils = () => {
+
+  return <div>Quiz</div>;
+}
+export default utils;
